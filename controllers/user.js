89372e9c676b637cbbb9c80b61c4b13a8985bfa3
bergmaner <bergkacper@gmail.com>
2020-09-11 04:44:01 +0200

    errorHandler

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,19 @@
-const User = require("../models/user")
+const User = require("../models/user");
+const { errorHandler } = require("../helpers/errorHandler");
 
 exports.signup = (req, res) => {
-    console.log("req",req.body);
-      const user = new User(req.body);
-  user.save((err,user) => {
-      if(err){
-          return res.status(400).json({
-              err
-          })
-      }
-      res.json({
-          user
-      })
-  })
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: errorHandler(err)
+      });
+    }
+    user.salt = undefined;
+    user.hashed_password = undefined;
+
+    res.json({
+      user,
+    });
+  });
 };
