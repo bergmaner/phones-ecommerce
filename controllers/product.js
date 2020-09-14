83c7adcb8bd62f07bb37413cf56352fb339d451f
bergmaner <bergkacper@gmail.com>
2020-09-14 20:23:03 +0200

    product validation

--- controllers/product.js
+++ controllers/product.js
@@ -14,9 +14,25 @@ exports.create = (req, res) => {
               error: "Image could not be uploaded"
           })
       }
+
+      const {name,description, price, category, quantity, shipping} = fields;
+
+      if(!name || !description || !price || !category || !quantity || !shipping ){
+          return res.status(400).json({
+              error: "All fields are required"
+          })
+      }
+
       let product = new Product(fields);
 
       if(files.image){
+
+        if(files.image.size > 1000000){
+            return res.status(400).jsonn({
+                error: "Image should be less than 1MB"
+            })
+        }
+
           product.image.data = fs.readFileSync(files.image.path)
           product.image.contentType = files.image.type;
       }
