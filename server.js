78c3607c7ec48f7f1e271b6caffc8186efc53cc8
bergmaner <bergkacper@gmail.com>
2020-10-12 13:11:42 +0200

    prepare to deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const path = require('path');
 const cors = require("cors");
 const all_routes = require("express-list-endpoints");
 const expressValidator = require("express-validator");
@@ -43,7 +44,11 @@ app.use("/api", productRoutes);
 app.use("/api", braintreeRoutes);
 app.use("/api", orderRoutes);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
